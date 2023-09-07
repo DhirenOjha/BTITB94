29208295dc33683d3fda6586d09f030c16a83f31
Macintosh <macintosh@Dhiren-Ojha.local>
2023-09-07 21:55:01 +0530

    Added new line.

--- javaprogram.java
+++ javaprogram.java
@@ -2,6 +2,6 @@ class main{
     public static void main(String args[]){
         System.out.println("This is a test program.");
         System.out.println("First Commit Staged.");
-        
+        System.out.println("Secong Commit Staged");
     }
 }
